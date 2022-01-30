ce334820016ebf5da694ad9117108591098385ba
ymd65536 <ymd65536@gmail.com>
2022-01-30 13:10:06 +0900

    MacOS用Lambdaセットアップスクリプト

--- setup_lambda.sh
+++ setup_lambda.sh
@@ -3,5 +3,9 @@
 # remove
 rm cloudtech_quiz_bot.zip
 
+cd cloudtech_quiz_bot
+python -m pip install LINE-bot-sdk pynamodb -t .
+cd ..
+
 # compress
 zip -r cloudtech_quiz_bot.zip ./cloudtech_quiz_bot/*
