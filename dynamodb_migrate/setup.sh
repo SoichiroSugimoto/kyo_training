13aa6f295aa15c0ac8ed7854d1c16074d7c98fa3
ymd65536 <ymd65536@gmail.com>
2022-01-30 17:34:41 +0900

    コメント修正

--- dynamodb_migrate/setup.sh
+++ dynamodb_migrate/setup.sh
@@ -1,4 +1,3 @@
-#! /bin/bash
-
+#!/bin/bash
 cp .env_sample .env
 pip3 install pynamodb python-dotenv
--- line_rich_menu/setup.sh
+++ line_rich_menu/setup.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 cp .env_sample .env
 pip3 install python-dotenv
 pip3 install line-bot-sdk -t .
