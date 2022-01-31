f400ecb001521e2f28a67e8c75f535cba1ff7e89
Kento.Yamada <ymd65536@gmail.com>
2022-01-31 19:35:24 +0900

    圧縮方法に誤りがったので修正

--- setup_lambda.sh
+++ setup_lambda.sh
@@ -5,7 +5,8 @@ rm cloudtech_quiz_bot.zip
 
 cd cloudtech_quiz_bot
 pip3 install LINE-bot-sdk pynamodb -t .
-cd ..
 
 # compress
-zip -r cloudtech_quiz_bot.zip ./cloudtech_quiz_bot/*
+zip -r ../cloudtech_quiz_bot.zip ./*
+
+cd ..
